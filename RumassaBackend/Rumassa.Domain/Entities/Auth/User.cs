548b606858b39a06262f0ab352752fb7c7f0e28f
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-05-02 07:05:40 +0500

    Identity User added close #18

--- RumassaBackend/Rumassa.Domain/Entities/Auth/User.cs
+++ RumassaBackend/Rumassa.Domain/Entities/Auth/User.cs
@@ -1,21 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rumassa.Domain.Entities
+namespace Rumassa.Domain.Entities.Auth
 {
-    public class User
+    public class User : IdentityUser<Guid>
     {
-
-        public int UserId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Email { get; set; }
         public string Password { get; set; }
-        public string Phone { get; set; }
-        public string PhotoPath { get; set; }
-
+        public string ProfilePhotoPath { get; set; }
     }
 }
