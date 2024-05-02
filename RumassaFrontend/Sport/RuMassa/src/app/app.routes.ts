85519a9e8ac8adba810e58508fa9be0216884b82
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-05-02 16:19:57 +0500

    f

--- RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
+++ RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NotFound404Component } from './components/pages/not-found404/not-found404.component';
 import { ObmeniComponent } from './components/pages/obmeni/obmeni.component';
+import { OplataComponent } from './components/pages/oplata/oplata.component';
 
 export const routes: Routes = [
     {path:'', redirectTo: '/home', pathMatch: 'full'},
     {path:'home', component:HomeComponent},
+    {path:'oplata', component:OplataComponent},
     {path:'obmeni', component:ObmeniComponent},
     {path:'**', component:NotFound404Component}
 ];
---
+++ RumassaFrontend/Sport/RuMassa/src/app/components/pages/oplata/oplata.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oplata',
+  standalone: true,
+  imports: [],
+  templateUrl: './oplata.component.html',
+  styleUrl: './oplata.component.scss'
+})
+export class OplataComponent {
+
+}
