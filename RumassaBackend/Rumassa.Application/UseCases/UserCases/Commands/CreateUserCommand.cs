27ed5407cc7344aff0f31d59bdfc425d51283fb7
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-05-02 10:50:16 +0500

    struct only close #4

---
+++ RumassaBackend/Rumassa.Application/UseCases/UserCases/Commands/CreateUserCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Rumassa.Domain.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumassa.Application.UseCases.UserCases.Commands
+{
+    public class CreateUserCommand : IRequest<ResponseModel>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ProfilePhotoPath { get; set; }
+    }
+}
