08d5f1155c7fdbe2584772444854ae53e50bbd86
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-05-02 10:24:43 +0500

    see

---
+++ RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NotFound404Component } from './components/pages/not-found404/not-found404.component';
+
+export const routes: Routes = [
+    {path:'', component:HomeComponent},
+    {path:'**', component:NotFound404Component}
+];
