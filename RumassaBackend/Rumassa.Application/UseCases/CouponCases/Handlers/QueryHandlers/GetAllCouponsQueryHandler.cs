694d134242127e1eb8c61932ff89fab6626a8e61
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-05-02 15:24:08 +0500

    CouponCases created

---
+++ RumassaBackend/Rumassa.Application/UseCases/CouponCases/Handlers/QueryHandlers/GetAllCouponsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Rumassa.Application.Abstractions;
+using Rumassa.Application.UseCases.CouponCases.Queries;
+using Rumassa.Application.UseCases.OrderCases.Queries;
+using Rumassa.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumassa.Application.UseCases.CouponCases.Handlers.QueryHandlers
+{
+    public class GetAllCouponsQueryHandler : IRequestHandler<GetAllCouponsQuery, List<Coupon>>
+    {
+
+        private readonly IRumassaDbContext _context;
+
+        public GetAllCouponsQueryHandler(IRumassaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Coupon>> Handle(GetAllCouponsQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Coupons.ToListAsync(cancellationToken);
+        }
+
+    }
+}
