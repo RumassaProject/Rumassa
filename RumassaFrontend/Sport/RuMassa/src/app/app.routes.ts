8d84f8346579bfabcc4ac5ce40fa4c1c8ee364d9
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-05-02 15:42:51 +0500

    navbar

--- RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
+++ RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { NotFound404Component } from './components/pages/not-found404/not-found404.component';
 
 export const routes: Routes = [
-    {path:'', component:HomeComponent},
-    {path:'**', component:NotFound404Component}
+    { path:'', component:HomeComponent },
+    { path:'**', component:NotFound404Component }
 ];
