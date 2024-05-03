051154a496b813a030e70c56b0cc80025e3c937a
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-05-03 10:01:04 +0500

    test

--- RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
+++ RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { NotFound404Component } from './components/pages/not-found404/not-found404.component';
 import { ObmeniComponent } from './components/pages/obmeni/obmeni.component';
 import { OplataComponent } from './components/pages/oplata/oplata.component';
+import { RegistrationComponent } from './components/pages/registration/registration.component';
 
 export const routes: Routes = [
-    {path:'', redirectTo: '/home', pathMatch: 'full'},
-    {path:'home', component:HomeComponent},
-    {path:'oplata', component:OplataComponent},
-    {path:'obmeni', component:ObmeniComponent},
-    {path:'**', component:NotFound404Component}
+    { path:'', redirectTo: '/home', pathMatch: 'full' },
+    { path:'home', component: HomeComponent },
+    { path:'registration', component: RegistrationComponent },
+    { path:'oplata', component: OplataComponent },
+    { path:'obmeni', component: ObmeniComponent },
+    { path:'**', component: NotFound404Component }
 ];
