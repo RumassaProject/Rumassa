bda61c8224715898bb4633437b158d59eb84d9bd
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-05-04 02:31:14 +0500

    ds

--- RumassaFrontend/Sport/RuMassa/src/app/components/pages/home/home.component.ts
+++ RumassaFrontend/Sport/RuMassa/src/app/components/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SubscribeComponent } from '../../subscribe/subscribe.component';
+import { HomeTopComponent } from './home-top/home-top.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SubscribeComponent],
+  imports: [SubscribeComponent, HomeTopComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
