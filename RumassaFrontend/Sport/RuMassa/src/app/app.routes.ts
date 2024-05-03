591958ef68915c11cfffe24a340958249cb08c40
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-05-03 09:07:26 +0500

    voxu

--- RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
+++ RumassaFrontend/Sport/RuMassa/src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { NotFound404Component } from './components/pages/not-found404/not-found404.component';
 import { ObmeniComponent } from './components/pages/obmeni/obmeni.component';
 import { OplataComponent } from './components/pages/oplata/oplata.component';
+import { SkidkiComponent } from './components/pages/skidki/skidki.component';
 
 export const routes: Routes = [
     {path:'', redirectTo: '/home', pathMatch: 'full'},
     {path:'home', component:HomeComponent},
     {path:'oplata', component:OplataComponent},
     {path:'obmeni', component:ObmeniComponent},
+    {path:'skidki', component:SkidkiComponent},
+
     {path:'**', component:NotFound404Component}
+
 ];
