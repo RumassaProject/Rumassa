4ce0a5eb9c26ec2549eabf321ff4e288d9a75517
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-05-04 14:17:20 +0500

    test

--- RumassaFrontend/Sport/RuMassa/src/app/components/registration/registration.component.ts
+++ RumassaFrontend/Sport/RuMassa/src/app/components/registration/registration.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+  constructor(private router : Router,private fb: FormBuilder, private authService : AuthService){}
 
+  form!: FormGroup;
+  hide = true;
+
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      phoneNumber: ['', Validators.required],
+    });
+  }
+
+  register() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.authService.register(this.form.value).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.router.navigateByUrl('/login');
+      },
+      error: (err) => {
+        console.log(err.error.message);
+        alert(err.error.message)
+      }
+    });
+  }
 }
---
+++ RumassaFrontend/Sport/RuMassa/src/app/services/auth.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { Register } from '../interfaces/register';
+import { Response } from '../interfaces/response';
+import { Login } from '../interfaces/login';
+import { TokenModel } from '../interfaces/token-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl = environment.apiUrl;
+  constructor(private http : HttpClient,private router : Router) { }
+  tokenKey = "accessToken"
+
+  
+  register(data:Register) : Observable<Response>{
+    return this.http.post<Response>(`${this.apiUrl}Auth/Register`, data).pipe(
+      map((response)=>{
+        console.log(response)
+        if(response.isSuccess == true){
+          console.log("Registered");
+        }
+        this.router.navigateByUrl('/register')
+        return response;
+      })
+    );
+  }
+
+  login(data:Login) : Observable<TokenModel>
+  {
+    console.log("Hi!")
+    return this.http.post<TokenModel>(`${this.apiUrl}Auth/Login`, data).pipe(
+      map((response)=>{
+        if(response.isSuccess){
+          //localStorage.clear();
+          localStorage.setItem(this.tokenKey, response.token)
+        }
+        this.router.navigateByUrl('/home');
+        return response;
+      })
+    );
+  }
+
+  logOut(){
+    localStorage.removeItem(this.tokenKey);
+  }
+
+}
