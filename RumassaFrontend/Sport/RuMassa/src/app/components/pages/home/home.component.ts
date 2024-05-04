3c752f91f56808d70e9d0ce93bbc6e9908fd4aa5
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-05-04 12:40:53 +0500

    ff

--- RumassaFrontend/Sport/RuMassa/src/app/components/pages/home/home.component.ts
+++ RumassaFrontend/Sport/RuMassa/src/app/components/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { SubscribeComponent } from '../../subscribe/subscribe.component';
 import { HomeTopComponent } from './home-top/home-top.component';
 import { CardComponent } from '../card/card.component';
 import { VoprosiComponent } from '../voprosi/voprosi.component';
+import { ConsultationComponent } from '../consultation/consultation.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SubscribeComponent, HomeTopComponent, CardComponent, VoprosiComponent],
+  imports: [SubscribeComponent, HomeTopComponent, CardComponent, VoprosiComponent, ConsultationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
