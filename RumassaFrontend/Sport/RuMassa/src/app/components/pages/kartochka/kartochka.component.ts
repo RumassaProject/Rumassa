98e5fddd09f7be4d2268e1beb92a50f687e63106
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-05-04 17:32:01 +0500

    Fixed

--- RumassaFrontend/Sport/RuMassa/src/app/components/pages/kartochka/kartochka.component.ts
+++ RumassaFrontend/Sport/RuMassa/src/app/components/pages/kartochka/kartochka.component.ts
@@ -11,12 +11,17 @@ export class KartochkaComponent {
   value=0
   c1="exit_btn1"
   c2="exit_btn2"
+  c3="exit_btn3"
 delete1(){
   console.log("bello")
 this.c1="cc"
 }
 delete2(){
   this.c2="cc"
+
+}
+delete3(){
+  this.c3="cc"
 
 }
 minus(){
